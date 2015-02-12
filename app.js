ccdd593cd6815533f5799cda94328e0e158b257a
gskerry <gregory.skerry>
2015-02-12 15:41:21 +0000

    init nodemon

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/news', function (req, res) {
+  res.send('Hello. Welcome to the News Room!')
+})
+
+app.get('/Bio', function (req, res) {
+  res.send('Hello. Here\'s all about me!')
+})
+
 app.listen(3000, '0.0.0.0', function() {
 	console.log("server listening...")
 } );
