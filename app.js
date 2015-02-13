efd9e08fa375cbd2cd5679ce5b3654cc4a1c194e
gskerry <gregory.skerry>
2015-02-13 00:16:38 +0000

    init underscore, layouts, routes index module, etc

--- app.js
+++ app.js
@@ -2,22 +2,61 @@
 var express = require('express');
 var morgan = require('morgan')
 var swig = require('swig')
+var routes = require('./routes/')
 
 var app = express();
 
 swig.setDefaults({ cache: false });
 
-app.use(morgan('dev'))
+/* App USE
+Intercepts ALL requests to the app
+e.g. becuase there is an app.use that pulls in routes directory... any routes defined in the home app.js are BYPASSED.
+*/
+
+app.use(morgan('dev'));
+app.use('/', routes);
+
+// 'Static' files don't need custom handlers like dynamic pages do
+// express method for designating entire folders as static file sources 
+// makes everything in directory accessible via URI path
+// (as if it was a file path...)
+// Even though it IS NOT.
+app.use(express.static(__dirname + '/public'));
+
 
 app.engine('html', swig.renderFile);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
 
-app.get('/', function (req, res) {
-	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-	res.render( 'index', {title: 'Hall of Fame', people: people} );
+// *****************
+// Homepage Routes
+// ****************
+
+// app.get('/', function (req, res) {
+// 	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// 	res.render( 'index', {title: 'Hall of Fame', people: people} );
+// })
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
 })
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+})
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+})
+
+
+// ****************
+// Other Routes
+// ****************
 
 // app.get('/news', function (req, res) {
 //   res.send('Hello. Welcome to the News Room!')
@@ -30,3 +69,4 @@ app.get('/', function (req, res) {
 app.listen(3000, '0.0.0.0', function() {
 	console.log("server listening...")
 } );
+
